bc1f14441534c1e6c905c664af115996be1c3fe6
Temple Olowonigba <olowotemple@yahoo.com>
2021-11-12 14:01:44 +0100

    Our app now takes into use the static middleware to serve our static files.

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let persons = [
 ];
 
 app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
